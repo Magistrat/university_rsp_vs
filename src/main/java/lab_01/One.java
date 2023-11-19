314ffb31854e7af335ab2f47d2bf78efaf418afd
Magistrat <nisraton@gmail.com>
2023-11-19 19:14:56 +0300

    [refactor] done 1th task (lab. 1) (use method for get, add doc string)

--- src/main/java/lab_01/One.java
+++ src/main/java/lab_01/One.java
@@ -3,11 +3,20 @@ package lab_01;
 public class One {
     public static void main(String[] args) {
 
+        // 1. Даны 4 числа типа int. Сравнить их и вывести наименьшее на консоль.
+
         int one_numb = 2;
         int two_numb = 5;
         int three_numb = 8;
         int four_numb = 15;
 
+        int minimal_numb;
+        minimal_numb = getMinimalNumb(one_numb, two_numb, three_numb, four_numb);
+        System.out.println(minimal_numb);
+    }
+
+    public static int getMinimalNumb(int one_numb, int two_numb, int three_numb, int four_numb){
+        // Получение минимального числа
         int minimal_numb;
 
         if (one_numb <= two_numb && one_numb <= three_numb && one_numb <= four_numb) {
@@ -20,6 +29,6 @@ public class One {
             minimal_numb = four_numb;
         }
 
-        System.out.println(minimal_numb);
+        return minimal_numb;
     }
 }
