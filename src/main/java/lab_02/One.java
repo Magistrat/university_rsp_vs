064cd372e379b734b4b681b10bb6ed3628df35f0
Magistrat <nisraton@gmail.com>
2023-11-19 21:53:03 +0300

    [feature] done 1th task (lab. 2)

---
+++ src/main/java/lab_02/One.java
@@ -0,0 +1,14 @@
+package lab_02;
+
+public class One {
+    public static void main(String[] args) {
+
+        // 1. При помощи цикла for вывести на экран нечетные числа от 1 до 99
+        for (int i = 1; i < 100; i++) {
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
