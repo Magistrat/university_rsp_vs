c35731834ad229c8e76a76c71764a3771338c459
Magistrat <nisraton@gmail.com>
2023-11-21 23:43:30 +0300

    [feature] done 1th task (lab 5)

---
+++ src/main/java/lab_05/One.java
@@ -0,0 +1,26 @@
+package lab_05;
+
+import java.util.Random;
+
+public class One {
+    public static void main(String[] args){
+
+        // 1. Создайте массив размерностью 5 на 6
+        // и заполните его случайными числами (в диапазоне от 0 до 99).
+        // Выведите на консоль третью строку.
+
+        int[][] arrayFiveOnSix = new int[5][6];
+        Random random = new Random();
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                arrayFiveOnSix[i][j] = random.nextInt(100);
+            }
+        }
+
+        for (int j = 0; j < 6; j++) {
+            System.out.println(arrayFiveOnSix[2][j]);
+        }
+
+    }
+}
